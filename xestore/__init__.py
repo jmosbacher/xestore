ad5451b54ec44a86a1371e5253fab3aaa8e5a111
Yossi Mosbacher <joe.mosbacher>
2020-12-15 08:41:47 +0200

    Bump version: 0.1.0 → 0.1.1

--- xestore/__init__.py
+++ xestore/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Yossi Mosbacher"""
 __email__ = 'joe.mosbacher@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .settings import Config
 from .client import XeStore
